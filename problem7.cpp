cae484492e8b65d2b32ac2839bf0acf915013aba
aysedemirel <aysedemirel1996@gmail.com>
2018-01-23 20:49:06 +0300

    problem:7

--- problem7.cpp
+++ problem7.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+bool prime(int number)
+{
+
+	for (int i = 2; i <= sqrt(number); i++)
+	{
+		if (number%i == 0) return false;
+	}
+	return true;
+
+	
+}
+int main()
+{
+	int number=2,counter=0;
+
+	while (1)
+	{
+		if (prime(number))counter++;
+		if (counter == 10001)break;
+		number++;
+	}
+	cout << "counter: " << counter << "\nnumber:  " << number << endl;
+
+	system("pause");
+
+	return 0;
+}
 
